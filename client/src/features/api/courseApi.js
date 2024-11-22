e26b4711c9611a06215c43232091173ab988cd88
Sahil Rana <krishna@Mac.lan>
2024-11-22 18:16:08 +0530

    Final Commit

--- client/src/features/api/courseApi.js
+++ client/src/features/api/courseApi.js
@@ -1,6 +1,7 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
-const COURSE_API = 'http://localhost:8080/api/v1/course';
+const COURSE_API =
+  'https://lecture-management-system-0m9j.onrender.com/api/v1/course';
 
 export const courseApi = createApi({
   reducerPath: 'courseApi',
--- client/src/features/api/courseProgressApi.js
+++ client/src/features/api/courseProgressApi.js
@@ -1,6 +1,7 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
-const COURSE_PROGRESS_API = 'http://localhost:8080/api/v1/progress';
+const COURSE_PROGRESS_API =
+  'https://lecture-management-system-0m9j.onrender.com/api/v1/progress';
 
 export const courseProgressApi = createApi({
   reducerPath: 'courseProgressApi',
